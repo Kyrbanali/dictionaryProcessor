67ef19156ae061243e8918f0f936a7dadc223ac1
Kurbanali <arslanaliev.ruby@gmail.com>
2024-05-31 01:24:03 +0300

    + методы для логирования

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
+set_error_handler('errorHandler');
+set_exception_handler('exceptionHandler');
+
+function errorHandler($errno, $errstr, $errfile, $errline) {
+    logError("Error: [$errno] $errstr - $errfile:$errline");
+}
+
+function exceptionHandler($exception) {
+    logError("Uncaught exception: " . $exception->getMessage());
+}
+
+function logError($error) {
+    $logFile = __DIR__ . '/mysql.log';
+    error_log(date('Y-m-d H:i:s') . " - $error\n", 3, $logFile);
+}
+
 require_once "src/Pushers/FilePusher.php";
 require_once "src/DictionaryProcessor.php";
 
